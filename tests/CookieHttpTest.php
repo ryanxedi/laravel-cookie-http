67fdc27757f9a13ebca9e4857b29884dd1ebf58a
Ryan Howard <ryan@xedi.com>
2025-07-21 22:17:55 +0100

    Fix dependencies and update workflow

--- tests/CookieHttpTest.php
+++ tests/CookieHttpTest.php
@@ -5,6 +5,9 @@ namespace Ryanxedi\CookieHttp\Tests;
 use Orchestra\Testbench\TestCase;
 use Illuminate\Support\Facades\Http;
 use Ryanxedi\CookieHttp\CookieHttpServiceProvider;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Ryanxedi\CookieHttp\HttpCookieStore;
 
 class CookieHttpTest extends TestCase
 {
@@ -16,8 +19,9 @@ class CookieHttpTest extends TestCase
     public function test_cookie_jar_can_store_and_retrieve_cookie(): void
     {
         Http::cookieStore();
-        Http::withCookies()->get('https://httpbin.org/cookies/set?testcookie=value');
-        $value = Http::cookieGet('testcookie');
-        $this->assertEquals('value', $value);
+        $request = new Request('GET', 'https://example.com');
+        $response = new Response(200, ['Set-Cookie' => 'testcookie=value; Path=/; Domain=example.com']);
+        HttpCookieStore::jar()->extractCookies($request, $response);
+        $this->assertEquals('value', Http::cookieGet('testcookie'));
     }
 }
